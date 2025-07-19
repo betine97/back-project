63418cdc26b0aeeb50eeeb106a6d051ced7e4fdb
betine97 <eronbetinegithub@gmail.com>
2025-07-19 16:31:16 -0300

    reset nos imports

--- src/view/view.go
+++ src/view/view.go
@@ -1,13 +1,1 @@
 package view
-
-import (
-	"back-project/src/controller/dtos"
-	entity "back-project/src/model/entitys.go"
-)
-
-func ConvertDomainToResponse(resp *entity.CreateUser) dtos.NewUser {
-	return dtos.NewUser{
-		First_Name: resp.First_Name,
-		Email:      resp.Email,
-	}
-}
