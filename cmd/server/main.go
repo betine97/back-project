0cd6b5ed19fc734fe6230ccb9f172ffca1e83e5d
betine97 <eronbetinegithub@gmail.com>
2025-07-19 16:44:02 -0300

    ajustando imports libs

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,41 @@
 package main
+
+import (
+	"github.com/betine97/back-project.git/cmd/config"
+	"github.com/betine97/back-project.git/src/controller"
+	"github.com/betine97/back-project.git/src/controller/routes"
+	"github.com/betine97/back-project.git/src/model/persistence"
+	"github.com/betine97/back-project.git/src/model/service"
+	"github.com/betine97/back-project.git/src/model/service/crypto"
+	"github.com/gofiber/fiber/v2"
+	"go.uber.org/zap"
+	"gorm.io/gorm"
+)
+
+func main() {
+
+	_ = config.NewConfig()
+
+	db, err := config.NewDatabaseConnection()
+	if err != nil {
+		zap.L().Fatal("Failed to connect to database", zap.Error(err))
+	}
+
+	userController := initDependencies(db)
+
+	app := fiber.New()
+
+	routes.SetupRoutes(app, userController)
+
+	if err := app.Listen(":8080"); err != nil {
+		zap.L().Fatal("Failed to start server", zap.Error(err))
+	}
+
+}
+
+func initDependencies(database *gorm.DB) controller.ControllerInterface {
+	cryptoService := &crypto.Crypto{}
+	persistence := persistence.NewDBConnection(database)
+	service := service.NewServiceInstance(cryptoService, persistence)
+	return controller.NewControllerInstance(service)
+}
--- src/controller/routes/routes.go
+++ src/controller/routes/routes.go
@@ -1,1 +1,15 @@
 package routes
+
+import (
+	"github.com/betine97/back-project.git/src/controller"
+	"github.com/betine97/back-project.git/src/controller/middlewares"
+	"github.com/gofiber/fiber/v2"
+)
+
+func SetupRoutes(app *fiber.App, userController controller.ControllerInterface) {
+	app.Post("/register", middlewares.UserValidationMiddleware, userController.CreateUser)
+	app.Post("/login", userController.LoginUser)
+
+	api := app.Group("/api", middlewares.JWTProtected())
+	api.Get("/otherservice", middlewares.JWTClaimsRequired("role", "user"), userController.RequestOtherService)
+}
